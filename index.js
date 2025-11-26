390ffa61bc5a9b4442523d6bfbe644d477ee0b15
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-11-26 21:44:22 +0530

    Refactor user input validation and data fetching

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         
         if(username.trim() === ""){
             alert("Username cannot be empty!");
+            /* showNoData(); */
             stats.style.visibility = "hidden";
             stats.innerHTML = '';
             userInput.value = "";
@@ -23,10 +24,9 @@ document.addEventListener("DOMContentLoaded", function() {
         const regex = /^[a-zA-Z0-9][a-zA-Z0-9-_]{1,18}[a-zA-Z0-9]$/;
         const isValid = regex.test(username);
         if(!isValid){
-            alert("Invalid username!")
-            stats.style.visibility = "hidden";
-            userInput.value = "";
-            stats.innerHTML = '';
+            alert("Invalid username!");
+            showNoData();
+            
             return false;
         }
         return true;
@@ -62,20 +62,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
     function showNoData() {
-    stats.style.visibility = "visible";
+        stats.style.visibility = "visible";
 
-    nameCard.innerHTML = "No user found";
-    nameCard.style.cssText = "display: flex; justify-content: center;";
+        nameCard.innerHTML = "No user found";
 
-    queCard.innerHTML = "";
-    rankCard.innerHTML = "";
-    rateCard.innerHTML = "";
+        queCard.innerHTML = "";
+        rankCard.innerHTML = "";
+        rateCard.innerHTML = "";
 
-    eProg.innerHTML = "Easy";
-    mProg.innerHTML = "Medium";
-    hProg.innerHTML = "Hard";
+        eProg.innerHTML = "Easy";
+        mProg.innerHTML = "Medium";
+        hProg.innerHTML = "Hard";
 
-}
+    }
 
 
 
@@ -110,52 +109,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
-
-
-
-/* 
-acceptanceRate
-: 
-72.28
-contributionPoints
-: 
-830
-easySolved
-: 
-61
-hardSolved
-: 
-7
-mediumSolved
-: 
-56
-message
-: 
-"retrieved"
-ranking
-: 
-1134185
-reputation
-: 
-0
-status
-: 
-"success"
-submissionCalendar
-: 
-{1732579200: 1, 1732665600: 4, 1732838400: 4, 1732924800: 1, 1733097600: 4, 1733184000: 1, 1733356800: 2, 1733529600: 3, 1734048000: 1, 1734220800: 1, 1734912000: 6, 1735171200: 2, 1735257600: 1, 1735430400: 1, 1735689600: 2, 1744243200: 1, 1744848000: 2, 1745020800: 1, 1745107200: 1, 1745280000: 2, 1745366400: 2, 1745452800: 1, 1745539200: 1, 1745625600: 1, 1745712000: 1, 1745798400: 1, 1746748800: 1, 1746835200: 2, 1747094400: 1, 1747353600: 2, 1747440000: 1, 1747612800: 2, 1747699200: 1, 1747785600: 3, 1748390400: 2, 1748476800: 1, 1748563200: 2, 1748649600: 1, 1748736000: 1, 1752192000: 2, 1756252800: 2, 1756339200: 2, 1756425600: 1, 1756598400: 1, 1756684800: 3, 1756771200: 1, 1756857600: 1, 1756944000: 3, 1757030400: 1, 1757203200: 1, 1757289600: 1, 1757376000: 2, 1757548800: 3, 1757635200: 4, 1757721600: 9, 1757894400: 1, 1757980800: 1, 1758153600: 2, 1758240000: 3, 1758326400: 3, 1758412800: 3, 1758499200: 8, 1758585600: 4, 1758672000: 1, 1758758400: 4, 1758844800: 1, 1758931200: 2, 1759104000: 2, 1759190400: 2, 1759276800: 2, 1759363200: 4, 1759449600: 4, 1759536000: 6, 1759622400: 4, 1759708800: 1, 1759795200: 3, 1759881600: 1, 1759968000: 1, 1760054400: 10, 1760140800: 1, 1760227200: 6, 1760313600: 1, 1760400000: 1, 1760486400: 4, 1760572800: 3, 1760659200: 2, 1760745600: 10, 1760832000: 5, 1760918400: 2, 1761004800: 5, 1761091200: 2, 1761177600: 2, 1761264000: 3, 1761350400: 8, 1761436800: 1, 1761523200: 1, 1761609600: 1, 1761696000: 1, 1761782400: 3, 1761868800: 2, …}
-totalEasy
-: 
-915
-totalHard
-: 
-887
-totalMedium
-: 
-1956
-totalQuestions
-: 
-3758
-totalSolved
-: 
-124 */
